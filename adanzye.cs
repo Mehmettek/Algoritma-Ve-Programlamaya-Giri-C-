71f60229fbe3e6232a95b5c72b60187bf64e5447
MEHMET TEK <mehmet_tek@windowslive.com>
2019-05-17 11:56:10 +0300

    Create adanzye.cs

---
+++ adanzye.cs
@@ -0,0 +1,12 @@
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            char alfabe;
+            for(alfabe='a';alfabe<='z';alfabe++)
+            {
+             Console.Write (alfabe  + " ");
+            
+            }
+          }
+    }
