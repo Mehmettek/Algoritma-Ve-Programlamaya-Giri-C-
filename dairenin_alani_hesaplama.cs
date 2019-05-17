da6b2ef50713029f4ce13f612f7af8baf20ef339
MEHMET TEK <mehmet_tek@windowslive.com>
2019-05-17 11:43:01 +0300

    Create dairenin_alani_hesaplama.cs

---
+++ dairenin_alani_hesaplama.cs
@@ -0,0 +1,16 @@
+public class Program
+    {
+        public static void Main(string[] args)
+        { 
+             const double pi=3.14;
+            double yaricap=5;
+            double alan=0;
+            
+            alan=pi*yaricap*yaricap;
+            
+            
+            Console.WriteLine("Alan: {0}",alan);
+            
+  
+        }
+    }
