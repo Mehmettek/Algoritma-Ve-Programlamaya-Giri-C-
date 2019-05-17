edd4b43d0293a41a20e1e9d5dad76ba14bb2017b
MEHMET TEK <mehmet_tek@windowslive.com>
2019-05-17 11:26:38 +0300

    Create ucgende_ucuncu_aciyi_bulma.cs

---
+++ ucgende_ucuncu_aciyi_bulma.cs
@@ -0,0 +1,12 @@
+public class Program
+    {
+  public static void Main(string[] args)
+        {
+ double a1,a2,a3;
+ a2=90;
+ a1=30;
+ a3=180-(a1+a2);
+ Console.WriteLine("Üçüncü Açı"+a3);
+
+        }
+    }
