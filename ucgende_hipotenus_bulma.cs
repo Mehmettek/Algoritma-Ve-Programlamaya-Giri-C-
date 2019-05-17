3abe2ac076ab43cdb65f0e1c9c7d3cdbdbfb4715
MEHMET TEK <mehmet_tek@windowslive.com>
2019-05-17 11:32:01 +0300

    Create ucgende_hipotenus_bulma.cs

---
+++ ucgende_hipotenus_bulma.cs
@@ -0,0 +1,12 @@
+ public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            double kenar1,kenar2,hipotenus;
+            kenar1=4;
+            kenar2=3;
+            hipotenus=Math.sqrt((kenar1*kenar1)+(kenar2*kenar2));
+          Console.WriteLine("Hipotenus: "+hipotenus);
+      Console.ReadKey();
+        }
+    }
