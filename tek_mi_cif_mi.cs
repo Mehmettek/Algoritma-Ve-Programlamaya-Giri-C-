18e5311695d48f33b6b91b2ac47cf1ecabd4513e
MEHMET TEK <mehmet_tek@windowslive.com>
2019-05-20 09:28:18 +0300

    Create  tek_mi_cif_mi.cs

---
+++ tek_mi_cif_mi.cs
@@ -0,0 +1,15 @@
+ public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int sayi;
+            sayi=10;
+            bool status=false;
+            if(sayi % 2 ==0)
+               Console.WriteLine("Cift Sayı");
+            else
+                Console.WriteLine("Tek Sayı");
+           
+           
+        }
+    }
