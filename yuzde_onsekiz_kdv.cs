c516cf480f08f9985459aa9930713a68748f70bc
MEHMET TEK <mehmet_tek@windowslive.com>
2019-05-20 11:59:26 +0300

    Create yuzde_onsekiz_kdv.cs

---
+++ yuzde_onsekiz_kdv.cs
@@ -0,0 +1,11 @@
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double fiyat,kdvli_fiyat;
+            fiyat=100;
+            kdvli_fiyat=fiyat + (fiyat*18/100);
+       Console.WriteLine(kdvli_fiyat);
+        }
+    }
