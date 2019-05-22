e1b93fa94b817fb38cac0f215482ce40e90be0cf
MEHMET TEK <mehmet_tek@windowslive.com>
2019-05-22 11:54:15 +0300

    Rename example-11.cs to example-12.cs

---
+++ example-12.cs
@@ -0,0 +1,230 @@
+   string sehir;
+            Console.Write("sehir ismi giriniz");
+            sehir = Console.ReadLine();
+            switch (sehir)
+            { 
+                case "adana":Console.Write("plaka kodu"+01);
+	                break; 
+                case "adıyaman":Console.Write("plaka kodu"+02);
+                    break;
+                    case "afyon":Console.Write("plaka kodu"+03);
+                    break;
+                     case "ağrı":Console.Write("plaka kodu"+04);
+                    break;
+                     case "amasya":Console.Write("plaka kodu"+05);
+                    break;
+                     case "Ankara":Console.Write("plaka kodu"+06);
+                    break;
+                     case "Antalya":Console.Write("plaka kodu"+07);
+                    break;
+                     case "Artvin":Console.Write("plaka kodu"+08);
+                    break;
+                     case "Aydın":Console.Write("plaka kodu"+09);
+                    break;
+                     case "Balıkesir":Console.Write("plaka kodu"+10);
+                    break;
+                     case "Bilecik":Console.Write("plaka kodu"+11);
+                    break;
+                     case "bimgöl":Console.Write("plaka kodu"+12);
+                    break;
+                    case "bitlis":Console.Write("plaka kodu"+13);
+                    break;
+                    case "bolu":Console.Write("plaka kodu"+14);
+                    break;
+                    case "burdur":Console.Write("plaka kodu"+15);
+                    break;
+                    case "bursa":Console.Write("plaka kodu"+16);
+                    break;
+                    case "çanakkale":Console.Write("plaka kodu"+17);
+                    break;
+                    
+                    case "çankırı":Console.Write("plaka kodu"+18);
+                    break;
+                    
+                    case "çorum":Console.Write("plaka kodu"+19);
+                    break;
+                    
+                    case "denizli":Console.Write("plaka kodu"+20);
+                    break;
+                    
+                    case "diyarbakır":Console.Write("plaka kodu"+21);
+                    break;
+                    
+                    case "edirne":Console.Write("plaka kodu"+22);
+                    break;
+                    
+                    case "elazığ":Console.Write("plaka kodu"+23);
+                    break;
+                    
+                    case "erzincan":Console.Write("plaka kodu"+24);
+                    break;
+                    
+                    case "erzurum":Console.Write("plaka kodu"+25);
+                    break;
+                    
+                    case "eskişehir":Console.Write("plaka kodu"+26);
+                    break;
+                    
+                    case "gaziantep":Console.Write("plaka kodu"+27);
+                    break;
+                    
+                    case "giresun":Console.Write("plaka kodu"+28);
+                    break;
+                    
+                    case "gümüşhane":Console.Write("plaka kodu"+29);
+                    break;
+                    
+                    case "hakkari":Console.Write("plaka kodu"+30);
+                    break;
+                    
+                    case "hatay":Console.Write("plaka kodu"+31);
+                    break;
+                    
+                    case "ısparta":Console.Write("plaka kodu"+32);
+                    break;
+                    
+                    case "içel":Console.Write("plaka kodu"+33);
+                    break;
+                    
+                    case "istanbul":Console.Write("plaka kodu"+34);
+                    break;
+                    
+                    case "izmir":Console.Write("plaka kodu"+35);
+                    break;
+                    
+                    case "kars":Console.Write("plaka kodu"+36);
+                    break;
+                    
+                    case "kastamonu":Console.Write("plaka kodu"+37);
+                    break;
+                    
+                    case "kayseri":Console.Write("plaka kodu"+38);
+                    break;
+                    
+                    case "kırklareli":Console.Write("plaka kodu"+39);
+                    break;
+                    
+                    case "kırlehir":Console.Write("plaka kodu"+40);
+                    break;
+                    
+                    case "kocaeli":Console.Write("plaka kodu"+41);
+                    break;
+                    
+                    case "konya":Console.Write("plaka kodu"+42);
+                    break;
+                    
+                    case "kütahya":Console.Write("plaka kodu"+43);
+                    break;
+                    
+                    case "malatya":Console.Write("plaka kodu"+44);
+                    break;
+                    
+                    case "manisa":Console.Write("plaka kodu"+45);
+                    break;
+                    
+                    case "kahramanmaraş":Console.Write("plaka kodu"+46);
+                    break;
+                    
+                    case "mardin":Console.Write("plaka kodu"+47);
+                    break;
+                    
+                    case "muğla":Console.Write("plaka kodu"+48);
+                    break;
+                    
+                    case "muş":Console.Write("plaka kodu"+49);
+                    break;
+                    
+                    case "nevşehir":Console.Write("plaka kodu"+50);
+                    break;
+                    
+                    case "niğde":Console.Write("plaka kodu"+51);
+                    break;
+                    
+                    case "ordu":Console.Write("plaka kodu"+52);
+                    break;
+                    
+                    case "rize":Console.Write("plaka kodu"+53);
+                    break;
+                    
+                    case "sakarya":Console.Write("plaka kodu"+54);
+                    break;
+                    
+                    case "samsun":Console.Write("plaka kodu"+55);
+                    break;
+                    
+                    case "siirt":Console.Write("plaka kodu"+56);
+                    break;
+                    
+                    case "sinop":Console.Write("plaka kodu"+57);
+                    break;
+                    
+                    case "sivas":Console.Write("plaka kodu"+58);
+                    break;
+                    
+                    case "tekirdağ":Console.Write("plaka kodu"+59);
+                    break;
+                    
+                    case "tokat":Console.Write("plaka kodu"+60);
+                    break;
+                    
+                    case "trabzon":Console.Write("plaka kodu"+61);
+                    break;
+                    
+                    case "tunceli":Console.Write("plaka kodu"+62);
+                    break;
+                    
+                    case "şanlıurfa":Console.Write("plaka kodu"+63);
+                    break;
+                    
+                    case "uşak":Console.Write("plaka kodu"+64);
+                    break;
+                    
+                    case "van":Console.Write("plaka kodu"+65);
+                    break;
+                    
+                    case "yozgat":Console.Write("plaka kodu"+66);
+                    break;
+                    
+                    case "zonguldak":Console.Write("plaka kodu"+67);
+                    break;
+                    case "aksaray":Console.Write("plaka kodu"+68);
+                    break;
+                    case "bayburt":Console.Write("plaka kodu"+69);
+                    break;
+                    case "karaman":Console.Write("plaka kodu"+70);
+                    break;
+                    case "kırıkkale":Console.Write("plaka kodu"+71);
+                    break;
+                    case "batman":Console.Write("plaka kodu"+72);
+                    break;
+                     case "asda":Console.Write("plaka kodu"+73);
+                    break;
+                   
+                     case "şırnak":Console.Write("plaka kodu"+74);
+                    break;
+                   
+                     case "bartın":Console.Write("plaka kodu"+75);
+                    break;
+                   
+                     case "ardahan":Console.Write("plaka kodu"+76);
+                    break;
+                    case "ığdır":Console.Write("plaka kodu"+77);
+                    break;
+                   
+                     case "yalova":Console.Write("plaka kodu"+78);
+                    break;
+                   
+                     case "karabük":Console.Write("plaka kodu"+79);
+                    break;
+                    case "kilis":Console.Write("plaka kodu"+80);
+                    break;
+                     case "osmaniye":Console.Write("plaka kodu"+81);
+                    break;
+                    case "düzce":Console.Write("plaka kodu"+82);
+                    break;
+                    default: Console.Write("sehir ismini dogru girin");
+                    break;
+
+            }
+            Console.Read();
+        }
