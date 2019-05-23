4029d133c40e4076e4518096ef44fdcaa5741499
MEHMET TEK <mehmet_tek@windowslive.com>
2019-05-23 08:40:27 +0300

    Rename example-13.cs to example-14.cs

---
+++ example-14.cs
@@ -0,0 +1,8 @@
+		int v;
+            Console.Write("AKİM GİRİNİZ>>>");
+            int i = Convert.ToInt16(Console.ReadLine());
+            Console.Write("DİRENC GİRİNİZ>>>>");
+            int r = Convert.ToInt16(Console.ReadLine());
+            v = i * r;
+            Console.Write(" iletkenin kutupları arasındaki gerilim {0}", v);
+            Console.ReadKey();
