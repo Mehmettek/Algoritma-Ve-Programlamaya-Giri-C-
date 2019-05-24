1073bd05c248fe52536554228a368832266c9b4c
MEHMET TEK <mehmet_tek@windowslive.com>
2019-05-24 11:53:47 +0300

    Create example-18.cs

---
+++ example-18.cs
@@ -0,0 +1,23 @@
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tmp=0,a,b;
+
+            Console.Write("harf giriniz");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("harf giriniz");
+           b = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("sayilerin ilk hali a={0}   b={1}", a, b);
+           tmp = a;
+           a = b;
+           b = tmp;
+
+           Console.Write(" sayilarin yer dgiştirilmis hali    a={0}  b={1}", a, b);
+           Console.Read();
+            
+        }
+    }
+}
