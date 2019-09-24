a43e56f0b6278bc3642a74b595e927936c8c2aaf
MEHMET TEK <mehmet_tek@windowslive.com>
2019-09-24 12:00:47 +0300

    Create for-each_1.cs

---
+++ for-each_1.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int[] sayilar={1,2,3,4,5,6,7,8,9,0};
+        foreach (int sayi in sayilar)
+        {
+            Console.WriteLine(sayi);
+        }
+        Console.WriteLine();
+   
+	}
+}
