e01dd43cb5951ce25e951c848d3738435581458e
MEHMET TEK <mehmet_tek@windowslive.com>
2019-09-24 13:54:56 +0300

    Create example-26.cs

---
+++ example-26.cs
@@ -0,0 +1,11 @@
+       int adet=0;
+            for (int i = 0; i <=100; i+=2)
+            {
+                if (i % 9==0)
+                {
+                    adet++;
+                }
+            }
+            Console.Write("Cevap: {0}", adet);
+
+            Console.Read();
