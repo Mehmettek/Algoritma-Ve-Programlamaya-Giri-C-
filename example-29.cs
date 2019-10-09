8a29a60519c6baa6188b2f3ec92ca87f286c908e
MEHMET TEK <mehmet_tek@windowslive.com>
2019-10-09 08:55:50 +0300

    Create example-29.cs

---
+++ example-29.cs
@@ -0,0 +1,18 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int i;
+		
+		for(i=1; i<=100; i++)
+		{
+			if(i%13==0)
+			{
+				Console.WriteLine(i);
+			}
+		}
+		
+	}
+}
